a1fa1789ca01267c4ee3ba0fc1651c80f2c9fba1
justinreese160 <84814933+justinreese160@users.noreply.github.com>
2021-07-30 10:03:49 -0500

    model index

--- models/index.js
+++ models/index.js
@@ -3,18 +3,37 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { belongsTo } = require('./Product');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+    foreignKey: 'id'
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+        foreignKey: 'id'
+    })
+    // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+        through: {
+            model: ProductTag,
+            unique: false
+        }
+    })
+    // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+    through: {
+        model: ProductTag,
+        unique: false
 
-// Products belongToMany Tags (through ProductTag)
+    }
+})
 
-// Tags belongToMany Products (through ProductTag)
 
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
+    Product,
+    Category,
+    Tag,
+    ProductTag,
 };
